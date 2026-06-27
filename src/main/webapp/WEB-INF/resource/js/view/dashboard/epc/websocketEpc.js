28ca84aa7be04cc74309edc2141bdeb35e58ac80
Urim <marronnier31@gmail.com>
2026-06-27 19:04:03 +0900

    Reset page assets for JSP practice

--- src/main/webapp/WEB-INF/resource/js/view/dashboard/epc/websocketEpc.js
+++ src/main/webapp/WEB-INF/resource/js/view/dashboard/epc/websocketEpc.js
@@ -1,17 +1,1 @@
-var stompClient = null;
-
-function connect(){
-	var socket = new SockJS('/chat');
-	stompClient = Stomp.over(socket);
-	stompClient.connect({}, function(frame){
-		setConneted(true);
-		console.log('session Cennected : '+ frame);
-		stompClient.subscribe('/topic/user', function(message){
-			socketUser(JSON.parse(message.body));
-			});
-	});
-}
-
-function socketUser(message){
-	alert(message);
-}
+// JSP 화면 실습용으로 초기화한 파일입니다.
